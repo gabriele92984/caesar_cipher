c8bdf4aba7bf1afbd277b397b0349a9c88bc14a8
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2024-09-19 11:45:56 +0200

    Add user get methods

--- cipher.rb
+++ cipher.rb
@@ -1,15 +1,22 @@
-def caesar_cipher
+def get_user_text
   puts "Please write your text:"
   text = gets.chomp
+end
 
+def get_user_shift
   puts "Now enter the shift value:"
   shift = gets.chomp.to_i
+end
 
+user_text = get_user_text
+shift_value = get_user_shift
+
+def caesar_cipher(text, shift)
+  
   text_array = text.split("")
 
   encoded_array = text_array.map do |character|
     unicode_value = character.ord + shift
-
     if unicode_value < 32
       unicode_value = 126 - (32 - unicode_value - 1)
     elsif unicode_value > 126
@@ -20,5 +27,5 @@ def caesar_cipher
   puts encoded_array.join
 end
 
-caesar_cipher()
+caesar_cipher(user_text, shift_value)
 
